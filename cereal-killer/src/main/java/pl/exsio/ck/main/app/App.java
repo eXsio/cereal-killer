9c8ca728130ce1559630b83c149732ff66610ba1
eXsio <slawomir.dymitrow@escsa.pl>
2014-11-07 10:39:19 +0100

    spring context

--- cereal-killer/src/main/java/pl/exsio/ck/main/app/App.java
+++ cereal-killer/src/main/java/pl/exsio/ck/main/app/App.java
@@ -12,7 +12,13 @@ public class App {
     private static ApplicationContext ctx;
 
     public static void main(String[] args) {
-        App.ctx = new ClassPathXmlApplicationContext("context.xml");
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                App.ctx = new ClassPathXmlApplicationContext("context.xml");
+            }
+        });
     }
 
     public static ApplicationContext getContext() {
